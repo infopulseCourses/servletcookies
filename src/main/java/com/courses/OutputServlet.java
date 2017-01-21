4f46ac81df1fd4193d8004e9565a685fe612d6ce
stepan <stepankachan@infopulse.com>
2017-01-21 17:03:33 +0200

    Request Dispatcher

--- src/main/java/com/courses/OutputServlet.java
+++ src/main/java/com/courses/OutputServlet.java
@@ -7,25 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author Stepan
  */
-public class SessionServlet extends HttpServlet {
+public class OutputServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession(true);
-
+        HttpSession session = req.getSession();
         String name = (String) session.getAttribute("name");
-        if(name == null){
-            name = req.getParameter("name");
-            session.setAttribute("name", name);
-        }
 
-        RequestDispatcher rd = req.getRequestDispatcher("/output-servlet");
-        rd.forward(req, resp);
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body>");
+        writer.println("<h2>" + name + "</h2>");
+        writer.print("</body></html>");
+        writer.close();
 
     }
-
 }
