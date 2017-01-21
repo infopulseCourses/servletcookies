e448ea41c12cda55745990dede7c07453cb58664
stepan <stepankachan@infopulse.com>
2017-01-21 16:12:55 +0200

    Cookies

--- src/main/java/com/courses/MyServlet.java
+++ src/main/java/com/courses/MyServlet.java
@@ -1,6 +1,7 @@
 package com.courses;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,13 +12,29 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      String name = req.getParameter("name");
-      resp.setContentType("text/html");
+        Cookie[] cookie = req.getCookies();
+        String name = null;
+        if (cookie != null) {
+            for (Cookie cook : cookie) {
+                if (cook.getName().equals("name")) {
+                    name = cook.getValue();
+                }
+            }
+        }
 
-      PrintWriter writer = resp.getWriter();
-      writer.println("<html><body>");
-      writer.println(name);
-      writer.print("</body></html>");
-      writer.close();
+        if(name == null){
+            name =   req.getParameter("name");
+            Cookie cook = new Cookie("name",name);
+            cook.setMaxAge(5);
+            resp.addCookie(cook);
+        }
+
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body>");
+        writer.println("<h2>" + name + "</h2>");
+        writer.print("</body></html>");
+        writer.close();
     }
 }
